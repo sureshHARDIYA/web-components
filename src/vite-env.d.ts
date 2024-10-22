b5ee8bd5159f695909b582fac697ed908c224789
Suresh KUMAR Mukhiya <itsmeskm99@gmail.com>
2024-10-22 20:50:22 +0200

    build fix

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="vite/client" />
+declare module "react-syntax-highlighter";
+declare module "react-syntax-highlighter/dist/esm/styles/prism";
+declare module "react-syntax-highlighter/dist/cjs/styles/prism/tomorrow";
